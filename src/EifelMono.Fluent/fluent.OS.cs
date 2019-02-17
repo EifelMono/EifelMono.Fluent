75ed8f555489e3de2d34890880fc3d6e6375848f
Andreas Klapperich <andreas@klapperich.de>
2019-02-17 21:02:22 +0100

    NEW: Fluent OS Added

---
+++ src/EifelMono.Fluent/fluent.OS.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace EifelMono.Fluent
+{
+#pragma warning disable IDE1006 // Naming Styles
+    public static partial class fluent
+#pragma warning restore IDE1006 // Naming Styles
+    {
+        public static class OS
+        {
+            public static bool IsWindows
+                => RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+            public static bool IsOSX
+                => RuntimeInformation.IsOSPlatform(OSPlatform.OSX);
+            public static bool IsLinux
+                => RuntimeInformation.IsOSPlatform(OSPlatform.Linux);
+        }
+    }
+}
