7af501ccdfc2c6fbf293ee4eb47ddb0f4b52aef0
Andreas Klapperich <andreas@klapperich.de>
2019-03-07 21:54:39 +0100

    NEW: fluent.Try added

---
+++ src/EifelMono.Fluent/fluent/fluent.Try.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EifelMono.Fluent
+{
+#pragma warning disable IDE1006 // Naming Styles
+    public static partial class fluent
+#pragma warning restore IDE1006 // Naming Styles
+    {
+        public static void Try(Action tryAction, Action<Exception> catchAction = null, Action finallyAction= null)
+        {
+            try
+            {
+                tryAction?.Invoke();
+            }
+            catch (Exception ex)
+            {
+                catchAction?.Invoke(ex);
+            }
+            finally
+            {
+                finallyAction?.Invoke();
+            }
+        }
+    }
+}
