2aad658abc62d2c09b6fc5a37a0c0e0b65b99e36
Andreas Klapperich <andreas@klapperich.de>
2019-05-29 09:43:15 +0200

    FIX: Tests

--- src/EifelMono.Fluent.Test/FlowTests/WaitQueueValueTests.cs
+++ src/EifelMono.Fluent.Test/FlowTests/WaitQueueValueTests.cs
@@ -17,9 +17,9 @@ namespace EifelMono.Fluent.Test.FlowTests
 
         [Theory]
         [InlineData(new DayOfWeek[] { DayOfWeek.Monday })]
-        [InlineData(new DayOfWeek[] { DayOfWeek.Tuesday})]
+        [InlineData(new DayOfWeek[] { DayOfWeek.Tuesday })]
         [InlineData(new DayOfWeek[] { DayOfWeek.Wednesday })]
-        [InlineData(new DayOfWeek[] { DayOfWeek.Thursday})]
+        [InlineData(new DayOfWeek[] { DayOfWeek.Thursday })]
         [InlineData(new DayOfWeek[] { DayOfWeek.Friday })]
         [InlineData(new DayOfWeek[] { DayOfWeek.Saturday })]
         [InlineData(new DayOfWeek[] { DayOfWeek.Sunday })]
@@ -27,19 +27,52 @@ namespace EifelMono.Fluent.Test.FlowTests
         [InlineData(new DayOfWeek[] { DayOfWeek.Saturday, DayOfWeek.Friday })]
         [InlineData(new DayOfWeek[] { DayOfWeek.Monday, DayOfWeek.Tuesday, DayOfWeek.Wednesday, DayOfWeek.Thursday, DayOfWeek.Friday,
                 DayOfWeek.Saturday, DayOfWeek.Sunday})]
-        public async void WaitQueueValueTest(DayOfWeek[] waitValues)
+        public async void WaitQueueValue1Test(DayOfWeek[] waitValues)
         {
-            var v = new WaitQueueValue<DayOfWeek>();
-            _ = Task.Run(async () =>
+            for (int index = 0; index < 5; index++)
             {
-                await Task.Delay(TimeSpan.FromMilliseconds(100));
-                foreach (var dayOfWeek in fluent.Enum.Values<DayOfWeek>())
+                var v = new WaitQueueValue<DayOfWeek>();
+                _ = Task.Run(async () =>
                 {
-                    v.AddData(dayOfWeek);
-                }
-            });
-            var result = await v.WaitValuesAsync(waitValues, TimeSpan.FromSeconds(1));
-            Assert.True(result.Ok);
+                    await Task.Delay(TimeSpan.FromMilliseconds(100));
+                    foreach (var dayOfWeek in fluent.Enum.Values<DayOfWeek>())
+                    {
+                        v.AddData(dayOfWeek);
+                    }
+                });
+                var result = await v.WaitValuesAsync(waitValues, TimeSpan.FromSeconds(1));
+                Assert.True(result.Ok);
+            }
+        }
+        [Theory]
+        [InlineData(new DayOfWeek[] { DayOfWeek.Monday })]
+        [InlineData(new DayOfWeek[] { DayOfWeek.Tuesday })]
+        [InlineData(new DayOfWeek[] { DayOfWeek.Wednesday })]
+        [InlineData(new DayOfWeek[] { DayOfWeek.Thursday })]
+        [InlineData(new DayOfWeek[] { DayOfWeek.Friday })]
+        [InlineData(new DayOfWeek[] { DayOfWeek.Saturday })]
+        [InlineData(new DayOfWeek[] { DayOfWeek.Sunday })]
+        [InlineData(new DayOfWeek[] { DayOfWeek.Wednesday, DayOfWeek.Friday })]
+        [InlineData(new DayOfWeek[] { DayOfWeek.Saturday, DayOfWeek.Friday })]
+        [InlineData(new DayOfWeek[] { DayOfWeek.Monday, DayOfWeek.Tuesday, DayOfWeek.Wednesday, DayOfWeek.Thursday, DayOfWeek.Friday,
+            DayOfWeek.Saturday, DayOfWeek.Sunday})]
+        public async void WaitQueueValue2Test(DayOfWeek[] waitValues)
+        {
+            for (int index = 0; index < 5; index++)
+            {
+                var v = new WaitQueueValue<DayOfWeek>();
+                var task = v.WaitValuesAsync(waitValues, TimeSpan.FromSeconds(1));
+                _ = Task.Run(async () =>
+                {
+                    await Task.Delay(TimeSpan.FromMilliseconds(100));
+                    foreach (var dayOfWeek in fluent.Enum.Values<DayOfWeek>())
+                    {
+                        v.AddData(dayOfWeek);
+                    }
+                });
+                var result = await task;
+                Assert.True(result.Ok);
+            }
         }
     }
 }
