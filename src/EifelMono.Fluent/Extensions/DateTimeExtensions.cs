9699b2c3123c02bcd53c256f0d5dfaba4bd5913b
eifelmono <andreas@klapperich.de>
2020-05-24 14:26:36 +0200

    CHG: New Datetime extensions

---
+++ src/EifelMono.Fluent/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace EifelMono.Fluent.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static DateTime FirstDayOfYear(this DateTime thisValue)
+            => new DateTime(thisValue.Year, 1, 1);
+        public static DateTime LastDayOfYear(this DateTime thisValue)
+            => new DateTime(thisValue.Year, 12, DateTime.DaysInMonth(thisValue.Year, 12));
+
+        public static DateTime FirstDayOfMonth(this DateTime thisValue)
+            => new DateTime(thisValue.Year, thisValue.Month, 1);
+        public static DateTime LastDayOfMonth(this DateTime thisValue)
+            => new DateTime(thisValue.Year, thisValue.Month, DateTime.DaysInMonth(thisValue.Year, thisValue.Month));
+    }
+}
